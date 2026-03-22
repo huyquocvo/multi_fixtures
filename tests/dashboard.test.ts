aa07869e47e7267252cefd671a1df7f41742b557
huyquocvo <quochuy29t6@gmail.com>
2026-03-22 11:50:04 +0700

    change

--- tests/dashboard.test.ts
+++ tests/dashboard.test.ts
@@ -2,15 +2,12 @@ import { test, expect } from '../fixtures/my-fixtures';
 
 // tests/dashboard.spec.ts
 
-
 test('should display user name on dashboard', async ({ authenticatedPage, user }) => {
   // 'authenticatedPage' is an already logged-in page
   // 'user' is the user data
-  
   await expect(authenticatedPage.getByRole('heading', { name: 'Home Dashboard' })).toBeVisible();
   // Your test logic here
 });
-
 test('another test using the same fixtures', async ({ authenticatedPage }) => {
   // Both fixtures (user, authenticatedPage) will be set up automatically
   await authenticatedPage.getByRole('button',{name: 'User Menu'}).click();
